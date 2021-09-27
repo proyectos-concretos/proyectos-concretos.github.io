912bbecacb6d310afd850b8af207a4f673ae82ea
daniel-altamar <dapeal42@gmail.com>
2021-09-27 10:33:46 -0500

    Envio de correos v1

---
+++ js/correo.js
@@ -0,0 +1,13 @@
+function enviar() {
+    Email.send({
+        SecureToken : "10e35afd-186f-4fc4-9201-9e91ddcba51b",
+        To : 'proyectosconcretos3@gmail.com',
+        From : document.querySelector('#correo').value,
+        Subject : "Potencial Cliente desde la Pagina",
+        Body : `<strong>${document.querySelector('#nombre').value}</strong> está interesado en obtener mas información sobre <strong>Proyectos Concretos</strong>, podemos contactarlo al correo: ${document.querySelector('#correo').value}`
+    }).then(
+      message => alert('Mensaje enviado')
+    );
+}
+
+document.querySelector('.btn-enviar').addEventListener('click', enviar);
